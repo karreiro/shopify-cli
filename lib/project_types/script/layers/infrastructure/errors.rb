27fcdc7812434fc140b02d1e1e550d1059f25d52
Thenura <thenura.jayasinghe@shopify.com>
2021-10-14 13:43:52 -0400

    remove merge dups

--- lib/project_types/script/layers/infrastructure/errors.rb
+++ lib/project_types/script/layers/infrastructure/errors.rb
@@ -78,23 +78,6 @@ module Script
           end
         end
 
-        class APILibraryNotFoundError < ScriptProjectError
-          attr_reader :library_name
-          def initialize(library_name)
-            super()
-            @library_name = library_name
-          end
-        end
-
-        class LanguageLibraryForAPINotFoundError < ScriptProjectError
-          attr_reader :language, :api
-          def initialize(language:, api:)
-            super()
-            @language = language
-            @api = api
-          end
-        end
-
         class DeprecatedEPError < ScriptProjectError; end
         class EmptyResponseError < ScriptProjectError; end
         class InvalidResponseError < ScriptProjectError; end
