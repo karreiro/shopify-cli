b795a3c539b74fbc365d7a959ecc892611cace98
Guilherme Carreiro <karreiro@gmail.com>
2022-03-17 09:40:46 +0100

    .

---
+++ lib/shopify_cli/theme/syncer/json_update_handler.rb
@@ -0,0 +1,65 @@
+# frozen_string_literal: true
+
+require_relative "forms/apply_to_all"
+require_relative "forms/select_update_strategy"
+
+module ShopifyCLI
+  module Theme
+    class Syncer
+      module JsonUpdateHandler
+        def enqueue_json_updates(files, overwrite_json)
+          # Some files must be uploaded after the other ones
+          delayed_files = [
+            @theme["config/settings_schema.json"],
+            @theme["config/settings_data.json"],
+          ]
+
+          # Update remote JSON files and delays `delayed_files` update
+          files = files
+            .-(delayed_files)
+            .+(delayed_files)
+            .select { |file| !ignore_file?(file) && file_has_changed?(file) }
+
+          if overwrite_json == true
+            enqueue_updates(files)
+          else
+            # Handle conflicts when JSON files cannot be overwritten
+            handle_update_conflicts(files)
+          end
+        end
+
+        private
+
+        def handle_update_conflicts(files)
+          to_get = []
+          to_update = []
+          to_union_merge = []
+
+          apply_to_all = Forms::ApplyToAll.new(@ctx, files.size)
+
+          files.each do |file|
+            update_strategy = apply_to_all.value || ask_update_strategy(file)
+            apply_to_all.apply?(update_strategy)
+
+            case update_strategy
+            when :keep_remote
+              to_get << file
+            when :keep_local
+              to_update << file
+            when :remote_merge
+              to_union_merge << file
+            end
+          end
+
+          enqueue_get(to_get)
+          enqueue_updates(to_update)
+          enqueue_union_merges(to_union_merge)
+        end
+
+        def ask_update_strategy(file)
+          Forms::SelectUpdateStrategy.ask(@ctx, [], file: file).strategy
+        end
+      end
+    end
+  end
+end
