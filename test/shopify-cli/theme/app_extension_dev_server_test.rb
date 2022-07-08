4f85f58a25f26c947cdbcd0bc92eb3fb4e827c33
Jesse Tuglu <jesse.tuglu@shopify.com>
2022-07-08 10:03:16 -0400

    add more middleware tests

---
+++ test/shopify-cli/theme/app_extension_dev_server_test.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+require "test_helper"
+require "shopify_cli/theme/app_extension_dev_server"
+
+module ShopifyCLI
+  module Theme
+    class AppExtensionDevServerTest < Minitest::Test; end
+    # TODO: more checks when adding HostTheme
+  end
+end
